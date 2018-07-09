9939ff1e96e35ac417ce669ef70606600a13fa6c
the-duck <markspurgeon96@hotmail.com>
2018-07-09 16:00:57 +0200

    updated & new generate script

---
+++ generate_icons.sh
@@ -0,0 +1,3 @@
+iconutil -c icns -o generated_assets/skriptoinstaller.icns DesktopApp/skriptoinstaller.iconset &
+iconutil -c icns -o generated_assets/skriptofile.icns DesktopApp/skriptofile.iconset &
+iconutil -c icns -o generated_assets/skripto.icns DesktopApp/skripto.iconset &
